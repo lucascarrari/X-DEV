febf70a771f96d132f3e96f5c1d412cd15595995
Lucas Carrari <lucascarrari@gmail.com>
2024-06-09 11:34:48 -0300

    X-Dev s

--- src/js/index.js
+++ src/js/index.js
@@ -6,29 +6,32 @@ const cartoes = document.querySelectorAll(".cartao");
 
 btnAvancar.addEventListener("click", function () {
 
-    if (cartaoAtual === cartoes.length - 1) return;
+    const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
 
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
-
-
+    if (ehUltimoCartao) return;
+    esconderCartaoSelecionado();
     cartaoAtual++;
-
-    cartoes[cartaoAtual].classList.add("selecionado");
+    mostrarCartao();
 
 });
 
 
 btnVoltar.addEventListener("click", function () {
+    
+    const ehPrimeiroCartao = cartaoAtual === 0;
 
-    if (cartaoAtual === 0) return;
-
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
-
-
+    if (ehPrimeiroCartao) return;
+    esconderCartaoSelecionado();
     cartaoAtual--;
-
-    cartoes[cartaoAtual].classList.add("selecionado");
+    mostrarCartao();
 
 });
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
